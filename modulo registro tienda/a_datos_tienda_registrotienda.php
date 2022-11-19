bc838da1e2a452f13e881e37b79f646faff0a079
Obed-HC <83016467+Obed-HC@users.noreply.github.com>
2022-11-19 16:35:50 -0500

    Cambios Api

--- modulo registro tienda/a_datos_tienda_registrotienda.php
+++ modulo registro tienda/a_datos_tienda_registrotienda.php
@@ -1,11 +1,11 @@
 <?php
-include '../conexion.php';
+include 'conexion.php';
 
 $json=array();
     if($_SERVER['REQUEST_METHOD']=='POST'){
         
         $p_tienombre=$_POST['p_tieNombre'];
-        $p_tieImagen=$_POST['p_tieImagen'];
+        $p_tieimagen=$_POST['p_tieImagen'];
         $p_tieURLWeb=$_POST['p_tieURLWeb'];
         $p_tieDescripcion=$_POST['p_tieDescripcion'];
         $p_tieCorreo=$_POST['p_tieCorreo'];
@@ -20,7 +20,21 @@ $json=array();
         $p_tieLatitud=$_POST['p_tieLatitud'];
         $p_tieLongitud=$_POST['p_tieLongitud'];
 
-        $insert="CALL sp_a_datos_tienda_registrotienda('{$p_tienombre}','{$p_tieImagen},'{$p_tieURLWeb}','{$p_tieDescripcion}','{$p_tieCorreo}','{$p_tieTelefono}','{$p_tieDireccion}','{$p_tieCiudad}','{$p_tieEstado}','{$p_tieVentasMensuales}','{$p_tieInventarioEstimado}','{$p_idDocumentoPersona}','{$p_idRubroTienda}','{$p_tieLatitud}','{$p_tieLongitud}')";
+
+        //Verificación de existencia de ruta imagen
+        if(empty($p_tieimagen)){
+            $imagePath = "";
+        }else{
+            //Generación de codigo único
+            $filePath = uniqid($p_idDocumentoPersona);
+            //Construimos la ruta de la imagen
+            $imagePath = "imgPerfilTienda/".$filePath.".jpeg";
+            //Insertando imagen en el directorio del servidor
+            file_put_contents($imagePath, base64_decode($p_tieimagen));
+        }
+
+
+        $insert="CALL sp_a_datos_tienda_registrotienda('{$p_tienombre}','{$imagePath}','{$p_tieURLWeb}','{$p_tieDescripcion}','{$p_tieCorreo}','{$p_tieTelefono}','{$p_tieDireccion}','{$p_tieCiudad}','{$p_tieEstado}','{$p_tieVentasMensuales}','{$p_tieInventarioEstimado}','{$p_idDocumentoPersona}','{$p_idRubroTienda}','{$p_tieLatitud}','{$p_tieLongitud}')";
         $resultado=mysqli_query($conexion,$insert);
         if($resultado){
             echo 'Registro exitoso de datos tienda';
@@ -28,5 +42,7 @@ $json=array();
         else{
             echo 'Fallo en Registrar Datos tienda';
         }
+        mysqli_close($conexion);
+        echo json_encode($json);
     }
 ?>
