a4fbe71b4da90658a7ad2d56b697750246ea761a
Obed-HC <83016467+Obed-HC@users.noreply.github.com>
2022-11-20 23:14:00 -0500

    Update m_datos_tienda_registrotienda.php

--- modulo registro tienda/m_datos_tienda_registrotienda.php
+++ modulo registro tienda/m_datos_tienda_registrotienda.php
@@ -17,22 +17,26 @@ $json=array();
         $p_tieLatitud=$_POST['p_tieLatitud'];
         $p_tieLongitud=$_POST['p_tieLongitud'];
         $p_idRubroTienda=$_POST['p_idRubroTienda'];
+        $p_idValidacionImagen=$_POST['p_idValidacionImagen'];
         
 
         //Verificación de existencia de ruta imagen
         if(empty($p_tieImagen)){
             $imagePath = "";
         }else{
-            //Generación de codigo único
-            $filePath = uniqid($p_idTienda);
-            //Construimos la ruta de la imagen
-            $imagePath = "imgPerfilTienda/".$filePath.".jpeg";
-            //Insertando imagen en el directorio del servidor
-            file_put_contents($imagePath, base64_decode($p_tieImagen));
+
+            if($p_idValidacionImagen=="1"){
+                //Generación de codigo único
+                $filePath = uniqid($p_idTienda);
+                //Construimos la ruta de la imagen
+                $imagePath = "imgPerfilTienda/".$filePath.".jpeg";
+                //Insertando imagen en el directorio del servidor
+                file_put_contents($imagePath, base64_decode($p_tieImagen));
+            }
         }
 
         //Actualizar Datos en la BD
-        $update="CALL sp_m_datos_tienda_registrotienda('{$p_idTienda}','{$p_tieNombre}','{$imagePath}','{$p_tieURLWeb}','{$p_tieDescripcion}','{$p_tieCorreo}','{$p_tieTelefono}','{$p_tieDireccion}','{$p_tieCiudad}','{$p_tieEstado}','{$p_tieLatitud}','{$p_tieLongitud}','{$p_idRubroTienda}')";
+        $update="CALL sp_m_datos_tienda_registrotienda('{$p_idTienda}','{$p_tieNombre}','{$imagePath}','{$p_tieURLWeb}','{$p_tieDescripcion}','{$p_tieCorreo}','{$p_tieTelefono}','{$p_tieDireccion}','{$p_tieCiudad}','{$p_tieEstado}','{$p_tieLatitud}','{$p_tieLongitud}','{$p_idRubroTienda}','{$p_idValidacionImagen}')";
         $resultado=mysqli_query($conexion,$update);
         if($resultado){
             echo 'Actualizacion Correcta';
