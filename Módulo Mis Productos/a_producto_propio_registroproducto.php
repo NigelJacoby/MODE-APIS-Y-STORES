ecebe06e3d0f3503c47e40993a0edfc34894bf90
Obed-HC <83016467+Obed-HC@users.noreply.github.com>
2022-11-25 04:04:04 -0500

    Update a_producto_propio_registroproducto.php

--- Módulo Mis Productos/a_producto_propio_registroproducto.php
+++ Módulo Mis Productos/a_producto_propio_registroproducto.php
@@ -12,7 +12,44 @@ $json=array();
         $p_idTienda=$_POST['p_idTienda'];
         $p_proDescripcion=$_POST['p_proDescripcion'];
 
-        $insert="CALL sp_a_producto_propio_registroproducto('{$p_lptImagen1}','{$p_lptImagen2}','{$p_lptImagen3}','{$p_lptPrecioCompra}','{$p_lptPrecioVenta}','{$p_idTienda}','{$p_proDescripcion}')";
+        //Verificación de existencia de ruta imagen
+        if(empty($p_lptImagen1)){
+            $imagePath1 = "";
+        }else{
+            //Generación de codigo único
+            $filePath1 = uniqid($p_idTienda);
+            //Construimos la ruta de la imagen
+            $imagePath1 = "imgProductosTienda/".$filePath1.".jpeg";
+            //Insertando imagen en el directorio del servidor
+            file_put_contents($imagePath1, base64_decode($p_lptImagen1));
+        }
+
+        //Verificación de existencia de ruta imagen
+        if(empty($p_lptImagen2)){
+            $imagePath2 = "";
+        }else{
+            //Generación de codigo único
+            $filePath2 = uniqid($p_idTienda);
+            //Construimos la ruta de la imagen
+            $imagePath2 = "imgProductosTienda/".$filePath2.".jpeg";
+            //Insertando imagen en el directorio del servidor
+            file_put_contents($imagePath2, base64_decode($p_lptImagen2));
+        }
+
+        //Verificación de existencia de ruta imagen
+        if(empty($p_lptImagen3)){
+            $imagePath3 = "";
+        }else{
+            //Generación de codigo único
+            $filePath3 = uniqid($p_idTienda);
+            //Construimos la ruta de la imagen
+            $imagePath3 = "imgProductosTienda/".$filePath3.".jpeg";
+            //Insertando imagen en el directorio del servidor
+            file_put_contents($imagePath3, base64_decode($p_lptImagen3));
+        }
+
+
+        $insert="CALL sp_a_producto_propio_registroproducto('{$imagePath1}','{$imagePath2}','{$imagePath3}','{$p_lptPrecioCompra}','{$p_lptPrecioVenta}','{$p_idTienda}','{$p_proDescripcion}')";
         $resultado=mysqli_query($conexion,$insert);
         if($resultado){
             echo 'Registro exitoso de Producto';
